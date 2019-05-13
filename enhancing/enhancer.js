1011e159c7cf9ae05dc15ac5e517ac410dd93e8b
brellin <cekakid@gmail.com>
2019-05-13 15:25:22 -0500

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,32 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: item.enhancement === 20 ?
+      20 :
+      item.enhancement + 1
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: item.enhancement < 15 ?
+      item.enhancement <= 5 ?
+        0 :
+        item.enhancement - 5 :
+      item.enhancement < 17 ?
+        item.enhancement - 10 :
+        item.enhancement - 1
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100
+  };
 }
 
 function get(item) {
---
+++ enhancing/enhancer.test.js
@@ -0,0 +1,143 @@
+const enhancer = require('./enhancer.js');
+
+// test away!
+
+let sword = {
+    name: 'Sword',
+    durability: 50,
+    enhancement: 1
+}
+
+describe('enhancer.js', () => {
+
+    // repair an item
+    describe('repair() function', () => {
+        it("should return an item's durability to 100", () => {
+            expect(enhancer.repair(sword)).toStrictEqual({
+                name: 'Sword',
+                durability: 100,
+                enhancement: 1
+            })
+        })
+    })
+
+    // succeed item enhancement
+    describe('succeed() function', () => {
+        it("should return an item's enhancement + 1", () => {
+            expect(enhancer.succeed(sword)).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 2
+            })
+            expect(enhancer.succeed({
+                ...sword,
+                enhancement: 10
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 11
+            })
+            expect(enhancer.succeed({
+                ...sword,
+                enhancement: 19
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 20
+            })
+        })
+
+        it("should return an item's enhancement to be 20 if already 20", () => {
+            expect(enhancer.succeed({
+                ...sword,
+                enhancement: 20
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 20
+            })
+        })
+    })
+
+    // fail item enhancement
+    describe('fail() function', () => {
+        it("should return an item's enhancement - 5 if under 15", () => {
+            expect(enhancer.fail(sword)).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 0
+            })
+            expect(enhancer.fail({
+                ...sword,
+                enhancement: 10
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 5
+            })
+            expect(enhancer.fail({
+                ...sword,
+                enhancement: 13
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 8
+            })
+        })
+
+        it("should return an item's enhancement - 10 if 15 or 16", () => {
+            expect(enhancer.fail({
+                ...sword,
+                enhancement: 15
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 5
+            })
+            expect(enhancer.fail({
+                ...sword,
+                enhancement: 16
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 6
+            })
+        })
+
+        it("should return an item's enhancement - 1 if 17 to 20", () => {
+            expect(enhancer.fail({
+                ...sword,
+                enhancement: 17
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 16
+            })
+            expect(enhancer.fail({
+                ...sword,
+                enhancement: 18
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 17
+            })
+            expect(enhancer.fail({
+                ...sword,
+                enhancement: 19
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 50,
+                enhancement: 18
+            })
+            expect(enhancer.fail({
+                ...sword,
+                enhancement: 20,
+                durability: 5
+            })).toStrictEqual({
+                name: 'Sword',
+                durability: 5,
+                enhancement: 19
+            })
+        })
+    })
+})
